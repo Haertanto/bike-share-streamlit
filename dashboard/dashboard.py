594324f03692ea807ebdd04c0457f214c01e4988
Haertanto <hertz.dhuar@gmail.com>
2024-11-17 09:48:43 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -61,22 +61,75 @@ max_date_days = days_df["dteday"].max()
 min_date_hour = hours_df["dteday"].min()
 max_date_hour = hours_df["dteday"].max()
 
-#Melengkapi Dashboard dengan Berbagai Visualisasi Data
-st.header('Bike Sharing DATA :sparkles:')
+with st.sidebar:
+    # Menambahkan logo perusahaan
+    st.image("https://storage.googleapis.com/gweb-uniblog-publish-prod/original_images/image1_hH9B4gs.jpg")
+    
+        # Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',
+        min_value=min_date_days,
+        max_value=max_date_days,
+        value=[min_date_days, max_date_days])
+  
+main_df_days = days_df[(days_df["dteday"] >= str(start_date)) & 
+                       (days_df["dteday"] <= str(end_date))]
 
-st.subheader("Jam paling banyak/sedikit penyewa?")
+main_df_hour = hours_df[(hours_df["dteday"] >= str(start_date)) & 
+                        (hours_df["dteday"] <= str(end_date))]
+
+hour_count_df = get_total_count_by_hour_df(main_df_hour)
+day_df_count_2011 = count_by_day_df(main_df_days)
+reg_df = total_registered_df(main_df_days)
+cas_df = total_casual_df(main_df_days)
+sum_order_items_df = sum_order(main_df_hour)
+season_df = macem_season(main_df_hour)
+
+#Melengkapi Dashboard dengan Berbagai Visualisasi Data
+st.header('Bike Sharing :sparkles:')
+
+st.subheader('Daily Sharing')
+col1, col2, col3 = st.columns(3)
+ 
+with col1:
+    total_orders = day_df_count_2011.count_cr.sum()
+    st.metric("Total Sharing Bike", value=total_orders)
+
+with col2:
+    total_sum = reg_df.register_sum.sum()
+    st.metric("Total Registered", value=total_sum)
+
+with col3:
+    total_sum = cas_df.casual_sum.sum()
+    st.metric("Total Casual", value=total_sum)
+
+st.subheader("Performa penjualan perusahaan dalam beberapa tahun terakhir")
+
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(
+    days_df["dteday"],
+    days_df["count_cr"],
+    marker='o', 
+    linewidth=2,
+    color="#90CAF9"
+)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+st.pyplot(fig)
+
+st.subheader("pada jam berapa yang paling banyak dan paling sedikit disewa?")
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 15))
 
-sns.barplot(x="hours", y="count_cr", data=sum_order_items_df.head(5), palette=["#000000", "#FF0000", "#00FF00", "#0000FF", "#00FFFF"], ax=ax[0])
+sns.barplot(x="hours", y="count_cr", data=sum_order_items_df.head(5), palette=["#D3D3D3", "#D3D3D3", "#90CAF9", "#D3D3D3", "#D3D3D3"], ax=ax[0])
 ax[0].set_ylabel(None)
-ax[0].set_xlabel("Jam (PM)", fontsize=30)
+ax[0].set_xlabel("Hours (PM)", fontsize=30)
 ax[0].set_title("Jam dengan banyak penyewa sepeda", loc="center", fontsize=30)
 ax[0].tick_params(axis='y', labelsize=35)
 ax[0].tick_params(axis='x', labelsize=30)
  
-sns.barplot(x="hours", y="count_cr", data=sum_order_items_df.sort_values(by="hours", ascending=True).head(5), palette=["#000000", "#FF0000", "#00FF00", "#0000FF", "#00FFFF"], ax=ax[1])
+sns.barplot(x="hours", y="count_cr", data=sum_order_items_df.sort_values(by="hours", ascending=True).head(5), palette=["#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3","#90CAF9"], ax=ax[1])
 ax[1].set_ylabel(None)
-ax[1].set_xlabel("Jam (AM)",  fontsize=30)
+ax[1].set_xlabel("Hours (AM)",  fontsize=30)
 ax[1].set_title("Jam dengan sedikit penyewa sepeda", loc="center", fontsize=30)
 ax[1].invert_xaxis()
 ax[1].yaxis.set_label_position("right")
@@ -85,9 +138,9 @@ ax[1].tick_params(axis='y', labelsize=35)
 ax[1].tick_params(axis='x', labelsize=30)
  
 st.pyplot(fig)
-st.subheader("Musim yang paling banyak penyewaan?")
+st.subheader("musim apa yang paling banyak disewa?")
 
-colors = ["#000000", "#FF0000", "#00FF00", "#0000FF"]
+colors = ["#D3D3D3", "#D3D3D3", "#D3D3D3", "#90CAF9"]
 fig, ax = plt.subplots(figsize=(20, 10))
 sns.barplot(
         y="count_cr", 
@@ -102,4 +155,17 @@ ax.set_xlabel(None)
 ax.tick_params(axis='x', labelsize=35)
 ax.tick_params(axis='y', labelsize=30)
 st.pyplot(fig)
+
+st.subheader("Perbandingan Customer yang Registered dengan casual")
+
+labels = 'casual', 'registered'
+sizes = [18.8, 81.2]
+explode = (0, 0.1) 
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',colors=["#D3D3D3", "#90CAF9"],
+        shadow=True, startangle=90)
+ax1.axis('equal')  
+
+st.pyplot(fig1)
 
